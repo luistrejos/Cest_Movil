4f7c1f559b5957832424fec7784f8cc168ea0586
luistrejos <luis.1701321107@ucaldas.edu.co>
2019-04-03 16:07:36 -0500

    Consulta por código QR
    Signed-off-by: luistrejos <luis.1701321107@ucaldas.edu.co>

--- CESTMobile/app/src/main/java/com/cest/cest_mobile/InfoElemento.java
+++ CESTMobile/app/src/main/java/com/cest/cest_mobile/InfoElemento.java
@@ -1,14 +1,10 @@
 package com.cest.cest_mobile;
 
-import android.app.Dialog;
 import android.content.DialogInterface;
-import android.graphics.Bitmap;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.textclassifier.TextClassification;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
