e4ab01911f227210c3d8baee3f3bfdedaaf0164c
victoresch <victoresch@hotmail.com>
2019-04-20 13:19:07 -0500

    Expresión regular validar correo 2

--- CESTMobile/app/src/main/java/com/cest/cest_mobile/InfoElemento.java
+++ CESTMobile/app/src/main/java/com/cest/cest_mobile/InfoElemento.java
@@ -113,7 +113,7 @@ public class InfoElemento extends AppCompatActivity {
                             dialogo.dismiss();
                         }
                         else if ( !correoaux.isEmpty() ){
-                            if (Patterns.EMAIL_ADDRESS.matcher(correoaux).matches()) ){
+                            if (Patterns.EMAIL_ADDRESS.matcher(correoaux).matches()){
                                 Toast.makeText(v.getContext(),"El reporte se guardado exitosamente, nos pondremos en contacto :" + correoaux, Toast.LENGTH_LONG).show();
                                 dialogo.dismiss();
                             }
